c5255cd1ac2d79e6ce9ec7d979ad17a7337a40af
Rahul Yadav <89911219+RahulY29@users.noreply.github.com>
2023-02-08 14:20:56 +0530

    name changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,76 @@
-import './App.css'
-import 'boxicons';
-import About from './components/About.jsx';
-import Skills from './components/Skills.jsx';
-import Projects from './components/Projects.jsx';
-import Contactus from './components/Contactus.jsx';
-import Footer from './components/Footer.jsx';
+import "./App.css";
+import "boxicons";
+import About from "./components/About.jsx";
+import Skills from "./components/Skills.jsx";
+import Projects from "./components/Projects.jsx";
+import Contactus from "./components/Contactus.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App() {
-  
   return (
     <main>
       {/** header start */}
       <header>
-    <a href="#" className="logo">Rahul <span>Yadav..</span></a>
+        <a href="#" className="logo">
+          Rahul <span>Yadav..</span>
+        </a>
 
-    <box-icon name="menu" id="menu-icon"></box-icon>
-    
-    <ul className="navbar">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#skills">Skills</a></li>
-        <li><a href="#proj">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <box-icon name="moon" id="darkmode"></box-icon>
-    </ul>
-</header>
+        <box-icon name="menu" id="menu-icon"></box-icon>
+
+        <ul className="navbar">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#proj">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <box-icon name="moon" id="darkmode"></box-icon>
+        </ul>
+      </header>
 
       {/** Home section */}
-    <section className="home" id="home">
-    <div className="social">
-        <a href="https://github.com/RahulY29"><box-icon class='github'></box-icon></a>
-        <a href="https://www.linkedin.com/in/rahul-yadav-461830223/"><box-icon class='linkedin'></box-icon></a>
-        <a href="#"><box-icon class='instagram'></box-icon></a>
-    </div>
-    <div className="home-img">
-        <img src="./src/images/RahulYadav.jpg" alt="main picture"/>
-    </div>
-    <div class="home-text">
-        <span>Hello, I'm</span>
-        <h1>Rahul Yadav</h1>
-        <h4>I am Passionate <span>Front-End Developer</span>.</h4>
-        <p>
-          
-        </p>
-        <a href="#about" className="btn">About me</a>
-    </div>
-</section>
+      <section className="home" id="home">
+        <div className="social">
+          <a href="https://github.com/RahulY29">
+            <box-icon class="github"></box-icon>
+          </a>
+          <a href="https://www.linkedin.com/in/rahul-yadav-461830223/">
+            <box-icon class="linkedin"></box-icon>
+          </a>
+          <a href="#">
+            <box-icon class="instagram"></box-icon>
+          </a>
+        </div>
+        <div className="home-img">
+          <img src="./src/images/rahulyadav.jpg" alt="main picture" />
+        </div>
+        <div class="home-text">
+          <span>Hello, I'm</span>
+          <h1>Rahul Yadav</h1>
+          <h4>
+            I am Passionate <span>Front-End Developer</span>.
+          </h4>
+          <p></p>
+          <a href="#about" className="btn">
+            About me
+          </a>
+        </div>
+      </section>
 
-      
-      <About/>
-      <Skills/>
-      <Projects/>
-      <Contactus/>
-      <Footer/>
+      <About />
+      <Skills />
+      <Projects />
+      <Contactus />
+      <Footer />
     </main>
-  )
+  );
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,44 +1,49 @@
 import "boxicons";
 
-const About = () =>{
-  return(
+const About = () => {
+  return (
     <main>
-    <section className="about" id="about">
-    <div className="heading">
-        <h2>About Me</h2>
-        <span>Introduction</span>
-    </div>
-
-    <div className="about-container">
-        <div className="about-img">
-            <img src="./src/images/RahulYadav.jpg" alt="aboutImage"/>
+      <section className="about" id="about">
+        <div className="heading">
+          <h2>About Me</h2>
+          <span>Introduction</span>
         </div>
-        <div className="about-text">
-            <p>I'm Front-End Developer with 6 Months of Training @ Quastech.
-                 I'm From INDIA. I code and create web elements for amazing people around the world. 
-                I like work with new people. New people new Experience</p>
+
+        <div className="about-container">
+          <div className="about-img">
+            <img src="./src/images/rahulyadav.jpg" alt="aboutImage" />
+          </div>
+          <div className="about-text">
+            <p>
+              I'm Front-End Developer with 6 Months of Training @ Quastech. I'm
+              From INDIA. I code and create web elements for amazing people
+              around the world. I like work with new people. New people new
+              Experience
+            </p>
             <div className="information">
-                <div className="info-box">
-                    <box-icon name='user'></box-icon>
-                    <span>Rahul Yadav</span>
-                </div>
+              <div className="info-box">
+                <box-icon name="user"></box-icon>
+                <span>Rahul Yadav</span>
+              </div>
 
-                <div className="info-box">
-                    <box-icon name='phone'></box-icon>
-                    <span>+91 8268143979</span>
-                </div>
+              <div className="info-box">
+                <box-icon name="phone"></box-icon>
+                <span>+91 8268143979</span>
+              </div>
 
-                <div className="info-box">
-                    <box-icon name="envelope"></box-icon>
-                    <span>yadavrahul2910@outlook.com</span>
-                </div>
+              <div className="info-box">
+                <box-icon name="envelope"></box-icon>
+                <span>yadavrahul2910@outlook.com</span>
+              </div>
             </div>
-            <a href="#contact" className="btn">Contact me!</a>
+            <a href="#contact" className="btn">
+              Contact me!
+            </a>
+          </div>
         </div>
-    </div>
-</section>
+      </section>
     </main>
-  )
-}
+  );
+};
 
 export default About;
